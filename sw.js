af8564fe8446b2bc00e8f2b7f9819374cb98e586
vedankayurvedawellness-cmd <vedankayurvedawellness@gmail.com>
2026-08-13 18:19:19 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// This background worker allows your web file to install like a real native app
+self.addEventListener('fetch', (event) => {
+  // Intentionally blank to fulfill install requirements
+});
